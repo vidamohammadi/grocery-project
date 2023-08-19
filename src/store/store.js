f6926ed2b48cfa7f539c6d5fd7c450b0dfc775ac
vida <m.vidamohammadi@gmail.com>
2023-08-19 14:04:57 -0700

    add vuex and store.js

---
+++ src/store/store.js
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+    state: {
+        // Your initial state properties go here
+    },
+    mutations: {
+        // Your mutation functions go here
+    },
+    actions: {
+        // Your action functions go here
+    },
+    getters: {
+        // Your getter functions go here
+    },
+});
+
+export default store;
